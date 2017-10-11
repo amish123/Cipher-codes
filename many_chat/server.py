d40865d34930ae56eb2ff92899d8edc157399019
amish gupta <amishgupta66@gmail.com>
2017-10-11 07:35:41 +0530

    Code tested::::Not for windows

--- many_chat/server.py
+++ many_chat/server.py
@@ -8,32 +8,32 @@ host=socket.gethostname()
 port=8081
 s.bind((host,port))
 s.listen(5)
-list_of_clients = []
-def clientthread(conn, addr):
-	conn.send(str.encode("Welcome to this chatroom!"))
+list= []
+def cthread(conn, addr):
+	conn.send(str.encode("Welcome to the chat"))
 	while 1:
 		try:
 			message = conn.recv(2048)
 			if message:
 				print("<" + addr[0] + "> " + message)
 				message2= "<" + addr[0] + "> " + message
-				for clients in list_of_clients:
+				for clients in list:
 					if clients!=conn:
 						try:
 							clients.send(message2)
 						except:
 							clients.close()
-							if clients in list_of_clients:
-								list_of_clients.remove(clients)
+							if clients in list:
+								list.remove(clients)
 					else:
-						if conn in list_of_clients:
-							list_of_clients.remove(conn)
+						if conn in list:
+							list.remove(conn)
 		except:
 			continue
 while True:
 	conn, addr = s.accept()
-	list_of_clients.append(conn)
+	list.append(conn)
 	print(addr[0] + " connected")
-	start_new_thread(clientthread,(conn,addr))    
+	start_new_thread(cthread,(conn,addr))    
 conn.close()
 server.close()
